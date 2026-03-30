2a75c427d5cb0bf2713af32c3e782d02d1acd62e
matz3ix <a.matsuura@seirei.ne.jp>
2026-03-30 17:56:10 +0900

    Delete src/main.gd

--- src/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _ready():
-    print("Welcome to the Seafood Rice Bowl Delivery Game!")
-    # Initialize game variables here
-    
-    # Set up game environment
-    setup_game_environment()
